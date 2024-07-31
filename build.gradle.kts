6f9a0af61dad604b8f5ad6efc4ec80a73afb3e98
Tarun-eware <tkala@efficiencyware.com>
2024-07-31 10:44:42 +0530

    dependencies in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,16 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.android.library") version "8.3.0" apply false
 }
+
+buildscript {
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+    }
+}
+
+ 
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
